fb6e91682348f3e9768c61ff0c9a9884b5c33fa3
yuya-yuya <yytyfp5115@icloud.com>
2022-06-09 19:29:00 +0900

    repositoryクラス作成

--- src/main/java/com/example/its/web/domain/issue/IssueService.java
+++ src/main/java/com/example/its/web/domain/issue/IssueService.java
@@ -1,17 +1,17 @@
 package com.example.its.domain.issue;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class IssueService {
 
+    private final com.example.its.domain.issue.IssueRepository issueRepository;
+
     public List<com.example.its.domain.issue.IssueEntity> findAll() {
-        return List.of(
-                new com.example.its.domain.issue.IssueEntity(1, "概要1", "説明1"),
-                new com.example.its.domain.issue.IssueEntity(2, "概要2", "説明2"),
-                new com.example.its.domain.issue.IssueEntity(3, "概要3", "説明3")
-        );
+        return issueRepository.findAll();
     }
 }
