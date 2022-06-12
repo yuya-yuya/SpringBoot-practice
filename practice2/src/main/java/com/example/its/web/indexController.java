53ca6cad4f21d1eebc0ba84ac3630d36c495d03c
yuya-yuya <yytyfp5115@icloud.com>
2022-06-12 14:57:00 +0900

    ログイン設定

--- practice2/src/main/java/com/example/its/web/indexController.java
+++ practice2/src/main/java/com/example/its/web/indexController.java
@@ -10,4 +10,9 @@ public class indexController {
     public String index() {
         return "index";
     }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
 }
