b940fd293ce13d01b6d9c0483444696de809815c
yuya-yuya <yytyfp5115@icloud.com>
2022-06-15 17:06:07 +0900

    userのコントローラーの修正とserviceの実装

---
+++ practice2/src/main/java/com/example/its/web/user/UserController.java
@@ -0,0 +1,23 @@
+package com.example.its.web.user;
+
+import com.example.its.domain.auth.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String showList(Model model) {
+        model.addAttribute("userList", userService.findAll());
+        return "users/list";
+    }
+
+}
