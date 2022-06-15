73f8efd2beb325f5dec6a71456b5f65f8449c894
yuya-yuya <yytyfp5115@icloud.com>
2022-06-15 16:36:53 +0900

    データ投入

--- practice2/src/main/java/com/example/its/web/config/SecurityConfig.java
+++ practice2/src/main/java/com/example/its/web/config/SecurityConfig.java
@@ -1,19 +1,39 @@
 package com.example.its.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        // for h2-console
+        http
+                .authorizeRequests().antMatchers("/h2-console/**").permitAll()
+                .and()
+                .csrf().ignoringAntMatchers("/h2-console/**")
+                .and()
+                .headers().frameOptions().disable();
         http
                 .authorizeRequests()
                 .mvcMatchers("/login/**").permitAll()
                 .and()
                 .formLogin()
                 .loginPage("/login");
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+                .passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
---
+++ practice2/src/main/java/com/example/its/web/domain/auth/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.example.its.domain.auth;
+
+import com.example.its.web.domain.auth.CustomUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        if ("tom".equals(username)) {
+            return new CustomUserDetails("tom", "password", Collections.emptyList());
+        }
+        throw new UsernameNotFoundException(
+                "Given username is not found. (username = '" + username + "')"
+        );
+    }
+}
